43a52f0f08ecf791d92dbceb93f55dfbf005cda0
Aditya Sekhar <saditya@ceiamerica.com>
2016-09-19 19:42:52 +0530

    Clean up code
    1. Remove hello world
    2. made login page first page

--- src/main/java/com/paydesk/data/UserRole.java
+++ src/main/java/com/paydesk/data/UserRole.java
@@ -10,6 +10,11 @@ import javax.persistence.Id;
 @Entity
 public class UserRole implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public UserRole() {
 
 	};
